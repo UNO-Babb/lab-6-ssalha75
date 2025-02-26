2709c966d2b5c74ee9e7166db0d022c43df8589d
ssalha75 <ssalha@unomaha.edu>
2025-02-26 15:26:03 +0000

    Done with DiceRoll.

--- DiceRoll.py
+++ DiceRoll.py
@@ -1,18 +1,31 @@
 #DiceRoll.py
-#Name:
-#Date:
-#Assignment:
+#Name: Sara Salha
+#Date: 2/26/25
+#Assignment:Lab 6
 import random
 
 def main():
   #Create an empty list with possible roll values
-  rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
+  rolls = [0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
-  
+  numRolls = 100000
+  for count in range (numRolls):
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    total = dice1 + dice2
+
+    rolls[total - 2]=rolls[total - 2] + 1
+
+    #print(dice1, dice2, total)
   #find the sum total of the two dice
   
   #print statictics for dice rolls
-
+  print(rolls)
+  num = 2
+  for r in rolls:
+    percent = round(r / numRolls * 100, 1)
+    print(num, ":", r, percent)
+    num = num + 1
 
 if __name__ == '__main__':
   main()
